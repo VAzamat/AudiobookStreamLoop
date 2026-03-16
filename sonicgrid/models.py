015128d38031cee7971e6045dd5997ac6cec0e59
Valeev Azamat <vazamat@gmail.com>
2026-03-16 20:09:06 +0300

    Publisher and Copyrighter/Rightholder class

--- sonicgrid/models.py
+++ sonicgrid/models.py
@@ -16,3 +16,36 @@ class Genre(models.Model):
 
     def __str__(self): return self.name
 
+class Publisher(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name="ID издательства")
+    name = models.CharField(max_length=255, verbose_name="Название издательства")
+    url = models.CharField(max_length=255, verbose_name="URL издательства", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Издательство"
+        verbose_name_plural = "Издательства"
+
+    def __str__(self): return self.name
+
+class Copyrighter(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name="ID копирайтера", blank=True)
+    name = models.CharField(max_length=255, verbose_name="Название копирайтера")
+    url = models.CharField(max_length=255, verbose_name="URL", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Копирайтер"
+        verbose_name_plural = "Копирайтеры"
+
+    def __str__(self): return self.name
+
+
+class Rightholder(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name="ID правообладателя")
+    name = models.CharField(max_length=255, verbose_name="Название")
+    url = models.CharField(max_length=255, verbose_name="URL", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Правообладатель"
+        verbose_name_plural = "Правообладатели"
+
+    def __str__(self): return self.name
